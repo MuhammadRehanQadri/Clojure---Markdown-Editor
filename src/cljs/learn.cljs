1f50b1521c0177912e24dc35615316674d66e8dd
MuhammadRehanQadri <rehan.code@gmail.com>
2021-11-12 17:55:39 +0500

    Markdownify completed

--- src/cljs/learn.cljs
+++ src/cljs/learn.cljs
@@ -3,31 +3,52 @@
    [goog.dom :as gdom]
    [reagent.core :as reagent :refer [atom]]
    [reagent.dom :as rdom]
-   [cljsjs.showdown :as showdown]))
+   [cljsjs.showdown]))
 
 
 ;; define your app data so that it doesn't get over-written on reload
 ;; (defonce app-state (atom {:text "Hello world!"}))
 
-
 (defonce markdown (reagent/atom ""))
+(defonce converter (js/showdown.Converter.))
 
-(defn get-app-element []
-  (gdom/getElement "app"))
+(defn markdown->html [markdown]
+  (.makeHtml converter markdown))
 
 (defn hello-world []
   [:div
-   [:p (prn-str @markdown)]
-   [:h3 "Markdown Editr"]
-   [:textarea
-    {:on-change #(reset! markdown (-> % .-target .-value))
-     :value @markdown}]])
+   [:h1
+    {:style {:text-align "center"
+             :color "green"}}
+    "Markdownify"]
+   [:div
+    {:style {:display "flex"}}
+    [:div
+     {:style {:flex "1"}}
+     [:h2
+      {:style {:text-align "center"}}
+      "Markdown Editor"]
+     [:textarea
+      {:on-change #(reset! markdown (-> % .-target .-value))
+       :style {:resize "none"
+               :width "100%"
+               :height "500px"}
+       :value @markdown}]]
+
+    [:div
+     {:style {:flex "1"
+              :padding-left "2em"}}
+     [:h2 
+      {:style {:text-align "center"}}
+      "HTML Preview"]
+     [:div {:dangerouslySetInnerHTML {:__html (markdown->html @markdown)}}]]]])
 
 
 
 
 
-
+(defn get-app-element []
+  (gdom/getElement "app"))
 
 
 (defn mount [el]
